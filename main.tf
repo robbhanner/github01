22919a62ec3965bfbfd3443c6dd1bc44052f9ec1
Robb Hanner <robbhanner@gmail.com>
2025-02-24 11:04:56 -0600

    removed azure sub ID info

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "azurerm" {
-  #  subscription_id = "c5d35f87-827d-4224-85d9-a5fac1b90b9b"
-  #  client_id       = "c99d3296-4fdd-4dd0-ad7e-6ac44293bf95"
-  #  client_secret   = "mzi8Q~R_Rx2Np6S.BBI5vOyi3CZMIT5MNL770b2Z"
-  #  tenant_id       = "9c8d7196-e4cf-4c75-9312-3b2fc26f41e6"
+  #  subscription_id = ""
+  #  client_id       = ""
+  #  client_secret   = ""
+  #  tenant_id       = ""
   features {}
 }
 
