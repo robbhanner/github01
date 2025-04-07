744231bfed62e28e0a2023e3c62e0862a582a98d
Robb Hanner <robbhanner@gmail.com>
2025-04-07 14:13:14 -0500

    public

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "azurerm_storage_account" "example" {
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  allow_blob_public_access = false
   allow_nested_items_to_be_public = false
 }
 
